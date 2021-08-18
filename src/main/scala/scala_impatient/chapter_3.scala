924f7a53c5c3462dc8966e3ba5e50a2f7f9fca0e
deepika-walmart <Deepika.Singh0@walmart.com>
2021-08-18 23:06:40 +0530

    Initial import

--- src/main/scala/scala_impatient/chapter_3.scala
+++ src/main/scala/scala_impatient/chapter_3.scala
@@ -6,13 +6,13 @@ import scala.io.StdIn._
 object chapter_3 {
 
   def q1_randomArray(n: Int):Array[Int]={
-    var a=new Array[Int](n)
-    for (i<- 0 to n-1) a(i)=Random.nextInt(n)
+    val a=new Array[Int](n)
+    for (i<- 0 until n) a(i)=Random.nextInt(n)
     a
   }
 
   def q2_swapArrayElements(a: Array[Int]): Array[Int] ={
-    for (i<- 0 until(a.length,2) if (i+1<a.length)) {
+    for (i<- 0 until(a.length,2) if i+1<a.length) {
       val t=a(i)
       a(i)=a(i+1)
       a(i+1)=t
@@ -21,7 +21,7 @@ object chapter_3 {
   }
 
   def q3_swapArrayYield(a:Array[Int])= {
-    for (i <- 0 until (a.length)) yield a(
+    for (i <- a.indices) yield a(
       if(i%2 ==0)
         if(i+1<a.length)
           i+1
@@ -37,12 +37,12 @@ object chapter_3 {
 
   def q4(a:Array[Int]): Array[Int] ={
     var j:Int=0
-    var b=new Array[Int](a.length)
-    for (i<- a.indices if (a(i)>0)) {
+    val b=new Array[Int](a.length)
+    for (i<- a.indices if a(i)>0) {
       b(j)=a(i)
       j+=1
     }
-    for (i<-a.indices if (a(i)<=0)) {
+    for (i<-a.indices if a(i)<=0) {
       b(j)=a(i)
       j+=1
     }
@@ -67,13 +67,21 @@ object chapter_3 {
   //Q7-remove duplicates
   def q7(a:Array[Int]):Array[Int]=a.distinct
 
+  //Q8-drop all negatives except the first one use 'drop'
+  def q8(a:ArrayBuffer[Int]):ArrayBuffer[Int]={
+    var b=for(i<-a.indices if a(i)<0) yield a(i)
+    b=b.drop(1)
+    for (i<-b.indices.reverse) a-=b(i)
+    a
+  }
+
   def main(args:Array[String]): Unit ={
     print("Q 1,2,3. Input size for a random array: ")
-    var a=q1_randomArray(readInt())
+    val a=q1_randomArray(readInt())
     for (i<-a) print(i+" ")
     println("")
 
-    var b=q2_swapArrayElements(a)
+    val b=q2_swapArrayElements(a)
     for (i<-b) print(i+" ")
     println("")
 
@@ -81,30 +89,30 @@ object chapter_3 {
 //    for (i<-a) print(i+" ")
 //    println("\nnew array:")
 
-    var c=q3_swapArrayYield(a)
+    val c=q3_swapArrayYield(a)
     for (i<-c) print(i+" ")
     println("")
 
     //Input new Array for q4
-    var x=readLine().split(" ").map(_.toInt)
-    var d=q4(x)
+    val x=readLine().split(" ").map(_.toInt)
+    val d=q4(x)
     for (i<-d) print(i+" ")
     println(" ")
 
     //Input an array of type double for q5
     println("Q5: Input a single line with elements of double type separated by single space")
-    var e=readLine().split(" ").map(_.toDouble)
-    var avg:Double= q5(e)
+    val e=readLine().split(" ").map(_.toDouble)
+    val avg:Double= q5(e)
     println("Calculated Average: "+avg)
 
     //Input array of type int for reverse sor (Q6)
     println("Q6: Input a single line with elements of type Int separated by single space")
-    var f=readLine().split(" ").map(_.toInt)
+    var f: Array[Int] =readLine().split(" ").map(_.toInt)
     f=q6_I(f)
     print("Reverse sorted array: ")
     for (i <- f) print(i+" ")
     println("")
-    var arrbuff=q6_II(collection.mutable.ArrayBuffer(f:_*))
+    val arrbuff=q6_II(collection.mutable.ArrayBuffer(f:_*))
     print("Reverse sorted ArrayBuffer: ")
     for(i<-arrbuff) print(i+" ")
     println("")
@@ -114,5 +122,16 @@ object chapter_3 {
     print("Array with removed duplicates: ")
     for (i<- q7(f)) print(i+" ")
     println("")
+
+    //Q8 Drop duplicates
+    println("Input an integer array with any no. of integers separated by single space")
+    f=readLine().split(" ").map(_.toInt)
+    print("Array with all negatives removed except the first one: ")
+    for (i<- q8(collection.mutable.ArrayBuffer(f:_*))) print(i+" ")
+    println("")
+
+    //Q9
+
+
   }
 }
